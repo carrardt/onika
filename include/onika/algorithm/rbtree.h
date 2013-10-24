4ca5d96a12299302d5254eea0ea71d5ecadfa878
Thierry Carrard <thierry@carrard.org>
2013-10-24 08:06:28 +0200

    passage au tuple pour le rbtree

--- include/onika/algorithm/rbtree.h
+++ include/onika/algorithm/rbtree.h
@@ -1,47 +1,108 @@
 #ifndef __onika_algorithm_rbtree_h
 #define __onika_algorithm_rbtree_h
 
+#include <tuple>
+#include <functional>   // std::less
+#include "onika/language.h"
 
 namespace onika { namespace algorithm {
 
-template<class _IntegerVector>
-struct rbtree_intvec_traits
+// RbTRee stored as an integer array, with least signficant bit used for color
+// tree composed of : [color|left|right|value] ... 
+template<class _TupleVector>
+struct RbTreeTupleVecBase
 {
-	typedef _IntegerVector Container;
-	typedef typename IntegerVector::value_type PointerType;
-	
-	static constexpr PointerType NullPtr = static_cast<PointerType>(0) - 1;
-	
-	static inline PointerType getLeft(const Container& tree, PointerType node)
+	typedef _TupleVector Container;
+	typedef typename Container::value_type Node; // position of node description in container
+	typedef typename std::tuple_element<3,NodeValue>::type ValueType; // position of node description in container
+	typedef size_t NodePtr; // position of node description in container
+
+	static constexpr NodePtr NullPtr = (static_cast<NodePtr>(0) - 1) >> 1;
+	static constexpr bool Black = false;
+	static constexpr bool Red = true;
+	static constexpr bool has_position = false;
+
+	inline RbTreeTupleVecBase() : root(NullPtr), freeNodes(NullPtr) {}
+
+	inline NodePtr   getRoot()           const { return root; }
+	inline bool      getColor(NodePtr i) const { return std::get<0>( tree[i] ) ; }
+	inline NodePtr   getLeft (NodePtr i) const { return std::get<1>( tree[i] ) ; }
+	inline NodePtr   getRight(NodePtr i) const { return std::get<2>( tree[i] ) ; }
+	inline ValueType getValue(NodePtr i) const { return std::get<3>( tree[i] ) ; }
+
+	inline void setRoot (NodePtr i)            	    { root = i; }
+	inline void setColor(NodePtr i, bool    x) 	    { std::get<0>(tree[i]) = x; }
+	inline void setLeft (NodePtr i, NodePtr x) 	    { std::get<1>(tree[i]) = x; }
+	inline void setRight(NodePtr i, NodePtr x) 	    { std::get<2>(tree[i]) = x; }
+	inline void setValue(NodePtr i, const ValueType& x) { std::get<3>(tree[i]) = x; }
+
+	// returns new freeNoseList
+	inline NodePtr addNode(bool color=Black, NodePtr left=NullPtr, NodePtr right=NullPtr)
 	{
-		return NullPtr;
-	}
-	
-	static inline PointerType getRight(const Container& tree, PointerType node)
-	{
-		return NullPtr;
+		NodePtr newNode;
+		if( freeNodes != NullPtr )
+		{
+			newNode = freeNodes;
+			freeNodes = getRight(tree,freeNodes);
+			tree[newNode] = Node(color,left,right);
+		}
+		else
+		{
+			newNode = tree.size();
+			tree.push_back( Node(color,left,right) );
+		}
+		return newNode;
 	}
 
-	static inline void setLeft(Container& tree, PointerType node, PointerType leftChild)
+	inline void removeNode(NodePtr i)
 	{
+		if( i == (tree.size()-1) )
+		{
+			tree.pop_back();
+		}
+		else
+		{
+			setRight(i,freeNodes);
+			freeNodes = i;
+		}
 	}
 
-	static inline void setRight(const Container& tree, PointerType node)
-	{
-	}
+	Container tree;
+	NodePtr root;
+	NodePtr freeNodes;
 };
 
-
-template<class _traits>
-struct RbTree
+template<class RbTreeBase, class _ValueCompare, bool has_position=RbTreeBase::has_position>
+struct RbTree : public RbTreeBase
 {
-	typedef _traits traits;
-	typedef traits::Container Container;
-	typedef traits::PointerType PointerType;
-	
-	
-	
-	Container& tree;
+	typedef _ValueCompare ValueCompare;
+	typedef RbTreeBase::NodePtr NodePtr;
+	typedef RbTreeBase::NodeValue NodeValue;
+	static constexpr NodePtr NullPtr = RbTreeBase::NullPtr;
+
+	struct NodeRef
+	{
+		NodePtr grandparent, parent, self;
+		RbTree& tree;
+		inline NodeRef(RbTree& t, NodePtr s, NodePtr p=NullPtr, NodePtr gp=NullPtr) : tree(t), self(s), parent(p), grandparent(gp) {}
+
+		inline bool getColor() { return tree.getColor(self); }
+		inline NodeRef getLeft() { return NodeRef( tree , tree.getLeft(self), self, parent ); }
+		inline NodeRef getRight() { return NodeRef( tree , tree.getRight(self), self, parent ); }
+
+		inline void setColor(bool x) { return tree.setColor(self,x); }
+		inline void setLeft(NodeRef x) { return tree.setLeft(self,x.self); }
+		inline void setRight(NodeRef x) { return tree.setRight(self,x.self); }
+	};
+
+	inline RbTree( const ValueCompare& comp ) : compare(comp) {}
+
+	inline NodeRef getRoot()
+	{
+		return NodeRef(tree,root);
+	}
+
+	ValueCompare compare;
 };
 
 } } // end of namespace
