07e6c3b6eaf270e7460794d1515eb9e22cffa20b
Thierry Carrard <thierry@carrard.org>
2013-10-31 18:26:41 +0100

    X

--- include/onika/algorithm/rbtree.h
+++ include/onika/algorithm/rbtree.h
@@ -2,113 +2,98 @@
 #define __onika_algorithm_rbtree_h
 
 #include <tuple>
+#include <vector>
 #include <functional>   // std::less
 #include "onika/language.h"
 
 namespace onika { namespace algorithm {
 
-// RbTRee stored as an integer array, with least signficant bit used for color
-// tree composed of : [color|left|right|value] ... 
-template<class _TupleVector>
-struct RbTreeTupleVecBase
+// TODO: add tree_traits
+template<class _ContainerType>
+struct TupleVectorBTree
 {
-	typedef _TupleVector Container;
-	typedef typename Container::value_type Node; // position of node description in container
-	typedef typename std::tuple_element<3,Node>::type ValueType; // position of node description in container
+	typedef _ContainerType ContainerType;
 	typedef size_t NodePtr; // position of node description in container
+	typedef typename ContainerType::value_type Node;
+	typedef typename std::tuple_element<0,Node>::type ValueType;
+	typedef typename std::tuple_element<1,Node>::type LeftPtrType;
+	typedef typename std::tuple_element<2,Node>::type RightPtrType;
 
 	static constexpr NodePtr NullPtr = (static_cast<NodePtr>(0) - 1) >> 1;
-	static constexpr bool Black = false;
-	static constexpr bool Red = true;
-	static constexpr bool has_position = false;
 
-	inline RbTreeTupleVecBase() : root(NullPtr), freeNodes(NullPtr) {}
+	inline TupleVectorBTree() : root(NullPtr), freelist(NullPtr) {}
 
-	inline NodePtr   getRoot()           const { return root; }
-	inline bool      getColor(NodePtr i) const { return std::get<0>( tree[i] ) ; }
-	inline NodePtr   getLeft (NodePtr i) const { return std::get<1>( tree[i] ) ; }
-	inline NodePtr   getRight(NodePtr i) const { return std::get<2>( tree[i] ) ; }
-	inline ValueType getValue(NodePtr i) const { return std::get<3>( tree[i] ) ; }
+	inline ValueType    getValue(NodePtr i) { return std::get<0>(data[i]) ; }
+	inline LeftPtrType  getLeft (NodePtr i) { return std::get<1>(data[i]) ; }
+	inline RightPtrType getRight(NodePtr i) { return std::get<2>(data[i]) ; }
 
-	inline void setRoot (NodePtr i)            	    { root = i; }
-	inline void setColor(NodePtr i, bool    x) 	    { std::get<0>(tree[i]) = x; }
-	inline void setLeft (NodePtr i, NodePtr x) 	    { std::get<1>(tree[i]) = x; }
-	inline void setRight(NodePtr i, NodePtr x) 	    { std::get<2>(tree[i]) = x; }
-	inline void setValue(NodePtr i, const ValueType& x) { std::get<3>(tree[i]) = x; }
+	inline void setValue(NodePtr i, ValueType x)    { std::get<0>(data[i]) = x; }
+	inline void setLeft (NodePtr i, LeftPtrType x)  { std::get<1>(data[i]) = x; }
+	inline void setRight(NodePtr i, RightPtrType x) { std::get<2>(data[i]) = x; }
 
 	// returns new freeNoseList
 	inline NodePtr addNode()
 	{
 		NodePtr newNode;
-		if( freeNodes != NullPtr )
+		if( freelist != NullPtr )
 		{
-			newNode = freeNodes;
-			freeNodes = getRight(freeNodes);
+			newNode = freelist;
+			freelist = getRight(freelist);
 		}
 		else
 		{
-			newNode = tree.size();
-			tree.resize( tree.size() + 1 );
+			newNode = data.size();
+			data.resize( data.size() + 1 );
 		}
-		setLeft(newNode,NullPtr);
-		setRight(newNode,NullPtr);
+		this->setLeft(newNode,NullPtr);
+		this->setRight(newNode,NullPtr);
 		return newNode;
 	}
 
 	inline void removeNode(NodePtr i)
 	{
-		if( i == (tree.size()-1) )
+		if( i == (data.size()-1) )
 		{
-			tree.pop_back();
+			data.pop_back();
 		}
 		else
 		{
-			setRight(i,freeNodes);
-			freeNodes = i;
+			setRight(i,freelist);
+			freelist = i;
 		}
 	}
 
-	Container tree;
+	ContainerType data;
 	NodePtr root;
-	NodePtr freeNodes;
+	NodePtr freelist;
+
 };
 
-template<class _TupleVector>
-struct RbTreeTupleVecBaseWithPosition : public RbTreeTupleVecBase<_TupleVector>
+template<class _ContainerType>
+struct TupleVectorPTree : public TupleVectorTree<_ContainerType>
 {
-	typedef RbTreeTupleVecBase<_TupleVector> Super;
-	typedef typename Super::Node Node; 
-	typedef typename Super::NodePtr NodePtr; 
-	typedef typename std::tuple_element<4,Node>::type PositionType; 
-	static constexpr bool has_position = true;
-
-	inline PositionType getPosition(NodePtr i) const { return std::get<4>(this->tree[i]) ; }
-	inline void setPosition(NodePtr i, PositionType p) { std::get<4>(this->tree[i]) = p ; }
+	typedef typename std::tuple_element<3,typename _ContainerType::value_type>::type CountType;
+	inline CountType getCount(NodePtr i) { return std::get<3>(data[i]) ; }
+	inline void setCount(NodePtr i, CountType x) { std::get<3>(data[i]) = x; }
 };
 
-template<class RbTree, bool has_position=RbTree::has_position> struct _NodeRef {};
-
-template<class RbTree>
-struct _NodeRef<RbTree,false>
+template<class Tree>
+struct _NodeRef<Tree>
 {
-	typedef typename RbTree::NodePtr NodePtr;
-	typedef typename RbTree::ValueType ValueType;
-	static constexpr NodePtr NullPtr = RbTree::NullPtr;
-	
+	typedef typename Tree::NodePtr NodePtr;
+	typedef typename Tree::ValueType ValueType;
+	static constexpr NodePtr NullPtr = Tree::NullPtr;
+
 	NodePtr grandparent, parent, self;
-	RbTree& tree;
-	
-	inline _NodeRef(RbTree& t, NodePtr s, NodePtr p=NullPtr, NodePtr gp=NullPtr) : tree(t), self(s), parent(p), grandparent(gp) {}
-	inline _NodeRef(RbTree& t) : tree(t), self( tree.addNode() ), parent(NullPtr), grandparent(NullPtr) {}
+	Tree& tree;
 
-	inline bool operator == (NodePtr i) const { return self == i; }
+	inline _NodeRef(Tree& t, NodePtr s, NodePtr p=NullPtr, NodePtr gp=NullPtr) : tree(t), self(s), parent(p), grandparent(gp) {}
+	inline _NodeRef(Tree& t) : tree(t), self( tree.addNode() ), parent(NullPtr), grandparent(NullPtr) {}
 
-	inline bool 	 getColor() const { return tree.getColor(self); }
 	inline _NodeRef  getLeft () const { return _NodeRef( tree , tree.getLeft(self), self, parent ); }
 	inline _NodeRef  getRight() const { return _NodeRef( tree , tree.getRight(self), self, parent ); }
 	inline ValueType getValue() const { return tree.getValue(self); }
 
-	inline void setColor(bool x) 		 { tree.setColor(self,x); }
 	inline void setLeft (_NodeRef x) 	 { tree.setLeft(self,x.self); }
 	inline void setRight(_NodeRef x) 	 { tree.setRight(self,x.self); }
 	inline void setValue(const ValueType& x) { tree.setValue(self,x); }
@@ -132,24 +117,64 @@ struct _NodeRef<RbTree,false>
 	}
 };
 
-template<class RbTree>
-struct _NodeRef <RbTree,true> : public _NodeRef<RbTree,false>
+#if 0
+// counter stored at each node is equal to the total number of nodes of it's left child's subtree 
+template<class RbTree, class count_traits >
+struct _PNodeRef : public _NodeRef<RbTree>
 {
-	typedef typename RbTree::PositionType PositionType;
-	inline PositionType getPosition() const { return this->tree.getPosition(this->self); }
-	inline void setPosition(const PositionType& x) { this->tree.setPosition(this->self,x); }
-};
+	typedef typename RbTree::ValueType ValueType;
+	typedef typename pos_traits::value_type CountType;
+	inline CountType getLeftCount() { return count_traits::get(this->tree.data,this->self); }
+	inline void setLeftCount(CountType n) { count_traits::set(this->tree.data,this->self,n); }
 
-template<class RbTreeBase, class _ValueCompare, bool has_position=RbTreeBase::has_position>
-struct RbTree : public RbTreeBase
+	inline _PNodeRef(RbTree& t, NodePtr s, _PNodeRef p, _PNodeRef gp ) : _NodeRef<RbTree>(t,s,p.self,gp.self)
+	{
+		index = getLeftCount();
+		if( this->parent != NullPtr )
+		{
+			if(  )
+			index += 1 + count_traits::get(this->tree.data,this->parent);
+		}
+	}
+	inline _PNodeRef(RbTree& t) : _NodeRef<RbTree>(t), index(0) { setLeftCount(0); }
+
+	inline _PNodeRef  getLeft () const { return _NodeRef( tree , tree.getLeft(self), self, parent ); }
+	inline _PNodeRef  getRight() const { return _NodeRef( tree , tree.getRight(self), self, parent ); }
+
+	inline _PNodeRef insert(const ValueType& x)
+	{
+		if( self == NullPtr )
+		{
+			this->self = tree.addNode();
+			this->setValue( x );
+			this->setLeftCount( 0 );
+		}
+		else if( tree.compare( x, getValue() ) )
+		{
+			this->setLeftCount( this->getLeftCount() + 1 );
+			this->setLeft( this->getLeft().insert(x) );
+		}
+		else
+		{
+			this->setRight( this->getRight().insert(x) );
+		}
+		return *this;
+	}
+	
+	CountType index;
+};
+#endif
+
+template<class BTreeBase, class _BTreeNodeRef=_NodeRef<BTree> class _ValueCompare>
+struct BTree : public BTreeBase
 {
 	typedef _ValueCompare ValueCompare;
-	typedef typename RbTreeBase::NodePtr NodePtr;
-	typedef typename RbTreeBase::ValueType ValueType;
-	typedef _NodeRef<RbTree,has_position> NodeRef;
-	static constexpr NodePtr NullPtr = RbTreeBase::NullPtr;
+	typedef typename BTreeBase::NodePtr NodePtr;
+	typedef typename BTreeBase::ValueType ValueType;
+	typedef _NodeRef<BTree> NodeRef;
+	static constexpr NodePtr NullPtr = BTreeBase::NullPtr;
 
-	inline RbTree( const ValueCompare& comp = ValueCompare() ) : compare(comp) {}
+	inline BTree( const ValueCompare& comp = ValueCompare() ) : compare(comp) {}
 
 	inline NodeRef getRoot()
 	{
@@ -183,9 +208,8 @@ struct RbTree : public RbTreeBase
 #include <vector>
 #include <functional>   // std::less
 
-typedef std::vector< std::tuple<bool,int,int,double> > TreeStorage;
-typedef onika::algorithm::RbTreeTupleVecBase< TreeStorage > SimpleTreeBase;
-typedef onika::algorithm::RbTree< SimpleTreeBase, std::less<double> > SimpleTree;
+typedef onika::algorithm::TupleVectorBTree< std::vector< std::tuple<double,int,int> > > SimpleTreeBase;
+typedef onika::algorithm::BTree< SimpleTreeBase, std::less<double> > SimpleTree;
 typedef typename SimpleTree::NodeRef NodeRef;
 
 void print(NodeRef node, int indent=0)
@@ -201,18 +225,36 @@ void print(NodeRef node, int indent=0)
 	print(node.getRight(),indent+4);
 }
 
+/*
+void print(PNodeRef node, int indent=0)
+{
+	for(int i=0;i<indent;i++) std::cout<<' ';
+	if( node == NodeRef::NullPtr ) 
+	{
+		std::cout<<"null\n";
+		return;
+	}
+	std::cout<<node.getValue()<<" @"<<node.self<<", P="<<node.getPosition()<<"\n";
+	print(node.getLeft(),indent+4);
+	print(node.getRight(),indent+4);
+}
+*/
+
 int main()
 {
+	int seed = 33;
+	seed = (std::ptrdiff_t)(&seed);
+	std::cout<<"seeding with "<<seed<<"\n";
+	srand48(seed);
+
 	SimpleTree t;
 	double value;
 	
-	
-	std::cin >> value;
-	while( value != 999 )
+	for(int i=0;i<10;i++)
 	{
-		t.insert( value );
-		std::cin >> value;
+		t.insert( drand48() );
 	}
+
 	print(t.getRoot());
 
 	return 0;
