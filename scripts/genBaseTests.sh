e5710def45b564bf5d3021becf630c9e578e4bc5
Thierry Carrard <thierry@carrard.org>
2013-11-07 20:43:44 +0100

    script pour convertir automatiquement les futurs .h en test unitaire associé

--- scripts/genBaseTests.sh
+++ scripts/genBaseTests.sh
@@ -1,9 +1,18 @@
 for HFILE in `find include -name "*.h"`
 do
 	INCFILE=`echo $HFILE | sed "s/include\///g"`
-	TESTDEF=`echo $INCFILE | sed "s/\//_/g" | sed "s/.h/_TEST/g"`
-	echo "#define $TESTDEF 1" > src/Tests/$TESTDEF.cpp
-	echo "#include \"$INCFILE\"" >> src/Tests/$TESTDEF.cpp
-	echo "" >> src/Tests/$TESTDEF.cpp
+	TESTDEF=`echo $INCFILE | sed "s/\//_/g" | sed "s/\.h/_TEST/g"`
+	INCFILESLASH=`echo $INCFILE | sed "s/\//_SLASH_/g"`
+	TESTFILE="src/Tests/$TESTDEF.cpp"
+	if [ ! -e $TESTFILE ]
+	then
+		echo "$HFILE"
+		cat $HFILE | sed s/\$/_NEWLINE_/g | tr -d "\n" | sed "s/^.*#ifdef $TESTDEF/#include \"$INCFILESLASH\"/g" \
+			   | sed "s/#endif.*#endif//g" | sed "s/_NEWLINE_/\n/g" | sed s/_SLASH_/\\\//g \
+			   > $TESTFILE
+		cat $HFILE | sed s/\$/_NEWLINE_/g | tr -d "\n" | sed "s/#ifdef $TESTDEF.*#endif/#endif/g" | sed "s/_NEWLINE_/\n/g" \
+			   > $HFILE.new
+		mv -f $HFILE.new $HFILE
+	fi
 done
 
