c44c80ca4dead17f602116e1c2804fc79947c281
Thierry Carrard <thierry@carrard.org>
2013-11-24 11:37:13 +0100

    ajout du mode de push par defaut

--- doc/git_config.sh
+++ doc/git_config.sh
@@ -1,3 +1,4 @@
-  git config --global user.email "thierry@carrard.org"
-  git config --global user.name "Thierry Carrard"
+git config --global user.email "thierry@carrard.org"
+git config --global user.name "Thierry Carrard"
+git config --global push.default simple
 
