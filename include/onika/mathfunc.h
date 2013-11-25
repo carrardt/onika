a8cddc2b93a544b474df172eaab8aadd93daaf85
Thierry Carrard <thierry@carrard.org>
2013-11-25 20:43:25 +0100

    Work still in progress. separate templates to avoid cross/unnecessary instanciations. template classes rather than template functions to explicit instantiation and avoid overloading rules confusion

--- include/onika/mathfunc.h
+++ include/onika/mathfunc.h
@@ -24,37 +24,29 @@ namespace std
 
 namespace onika { namespace math {
 
-
-	template<class T> struct TupleLength { static constexpr int value=-1; };
-	template<class... T> struct TupleLength< std::tuple<T...> > { static constexpr int value=sizeof...(T); };
-
-	// definition of math functions on simple scalars
-#define MATH_FUNC1(name,code) \
-		template<class T> static inline auto name##_s(const T& x) ONIKA_AUTO_RET( code )
-		MATH_FUNC1(negate,(-x))
-		MATH_FUNC1(inverse,(1/x))
-		MATH_FUNC1(abs,std::abs(x))
-		MATH_FUNC1(norm,std::abs(x))
-		MATH_FUNC1(norm2,(x*x))
+		//************* forward declaration of method selectors ****************
+#define MATH_FUNC1(name) template <class T> struct C##name##X;
+		MATH_FUNC1(negate)
+		MATH_FUNC1(inverse)
+		MATH_FUNC1(abs)
+		MATH_FUNC1(norm)
+		MATH_FUNC1(norm2)
 #undef 	MATH_FUNC1
-#define MATH_FUNC2(name,code) \
-		template<class T1,class T2> static inline auto name##_ss(const T1& x, const T2& y) ONIKA_AUTO_RET( code )
-		MATH_FUNC2(sub,(x-y))
-		MATH_FUNC2(add,(x+y))
-		MATH_FUNC2(mul,(x*y))
-		MATH_FUNC2(div,(x/y))
-		MATH_FUNC2(dot,(x*y))
-		MATH_FUNC2(distance,std::abs(y-x))
-		MATH_FUNC2(distance2,(y-x)*(y-x))
+#define MATH_FUNC2(name) template <class T1, class T2> struct C##name##X;
+		MATH_FUNC2(sub)
+		MATH_FUNC2(add)
+		MATH_FUNC2(mul)
+		MATH_FUNC2(div)
+		MATH_FUNC2(dot)
+		MATH_FUNC2(distance)
+		MATH_FUNC2(distance2)
 #undef 	MATH_FUNC2
-
-		template <class T> struct UnaryFunc;
-		template<class T1, class T2> struct BinaryFunc;
+		//********************************************************
 
 	//************* front-end methods definition ****************
 #define MATH_FUNC1(name) \
 		template <class T> static inline auto name(const T& x) \
-		ONIKA_AUTO_RET( UnaryFunc<T>::name##_selector(x) )
+		ONIKA_AUTO_RET( C##name##X<T>::name##X(x) )
 		MATH_FUNC1(negate)
 		MATH_FUNC1(inverse)
 		MATH_FUNC1(abs)
@@ -64,7 +56,7 @@ namespace onika { namespace math {
 
 #define MATH_FUNC2(name) \
 		template <class T1, class T2> static inline auto name(const T1& x, const T2& y) \
-		ONIKA_AUTO_RET( BinaryFunc<T1,T2>::name##_selector(x,y) )
+		ONIKA_AUTO_RET( C##name##XX<T1,T2>::name##XX(x,y) )
 		MATH_FUNC2(sub)
 		MATH_FUNC2(add)
 		MATH_FUNC2(mul)
@@ -77,58 +69,69 @@ namespace onika { namespace math {
 
 
 
+		//==========================================================
+		//== Scalar versions of unary and binary functions        ==
+		//==========================================================
+	#define MATH_FUNC1(name,code) \
+			template<class T> struct C##name##S { static inline auto name##S(const T& x) ONIKA_AUTO_RET( code ) };
+			MATH_FUNC1(negate,(-x))
+			MATH_FUNC1(inverse,(1/x))
+			MATH_FUNC1(abs,std::abs(x))
+			MATH_FUNC1(norm,std::abs(x))
+			MATH_FUNC1(norm2,(x*x))
+	#undef 	MATH_FUNC1
+	#define MATH_FUNC2(name,code) \
+			template<class T1,class T2> struct C##name##SS { static inline auto name##SS(const T1& x, const T2& y) ONIKA_AUTO_RET( code ) };
+			MATH_FUNC2(sub,(x-y))
+			MATH_FUNC2(add,(x+y))
+			MATH_FUNC2(mul,(x*y))
+			MATH_FUNC2(div,(x/y))
+			MATH_FUNC2(dot,(x*y))
+			MATH_FUNC2(distance,std::abs(y-x))
+			MATH_FUNC2(distance2,(y-x)*(y-x))
+	#undef 	MATH_FUNC2
+
+
+	// tuple size helper template
+	template<class T> struct TupleLength { static constexpr int value=-1; };
+	template<class... T> struct TupleLength< std::tuple<T...> > { static constexpr int value=sizeof...(T); };
+
+	// macros for tuple handling
 #define MT(x...) std::make_tuple(x)
 #define TA(x,y) std::tuple_cat(x,MT(y))
 #define TG(t,i) std::get<i>(t)
 #define TS(t) (TupleLength<t>::value)
-#define TMI TupleMathIterator
-#define TTMI TupleTupleMathIterator
-#define TSMI TupleScalarMathIterator
-#define PTMI TupleMathIterator<T,I-1>
-#define PTTMI TupleTupleMathIterator<T1,T2,I-1>
-#define PTSMI TupleScalarMathIterator<T1,T2,I-1>
 
-	template <class T,unsigned int I=TS(T)-1> struct TMI;
-	template <class T1, class T2, unsigned int I=TS(T1)-1 > struct TTMI;
-	template <class T1, class T2, unsigned int I=TS(T1)-1 > struct TSMI;
 
 	//==========================================================
 	//== Select tuple/non tuple operators for unary functions ==
 	//==========================================================
-	template <class T> struct UnaryFunc
-	{
 #define MATH_FUNC1(name) \
-		static inline auto name##_selector(const T& x) ONIKA_AUTO_RET( name##_s(x) )
+	template <class T> struct C##name##X { static inline auto name##X(const T& x) ONIKA_AUTO_RET( C##name##S::name##S(x) ) };
+	MATH_FUNC1(negate)
+	MATH_FUNC1(inverse)
+	MATH_FUNC1(abs)
+	MATH_FUNC1(norm)
+	MATH_FUNC1(norm2)
+#undef MATH_FUNC1
+
+#define MATH_FUNC1(name) \
+	template <class... T> struct C##name##X<std::tuple<T...> > \
+	{ static inline auto name##X(const T& x) ONIKA_AUTO_RET( C##name##T::name##T(x) ) };
 		MATH_FUNC1(negate)
 		MATH_FUNC1(inverse)
 		MATH_FUNC1(abs)
 		MATH_FUNC1(norm)
 		MATH_FUNC1(norm2)
 #undef MATH_FUNC1
-	};
-
-	template <class... T> struct UnaryFunc< std::tuple<T...> >
-	{
-#define MATH_FUNC1(name) \
-		static inline auto name##_selector(const std::tuple<T...>& x) ONIKA_AUTO_RET( TMI<std::tuple<T...> >::name##_t(x) )
-		MATH_FUNC1(negate)
-		MATH_FUNC1(inverse)
-		MATH_FUNC1(abs)
-#undef MATH_FUNC1
-		static inline auto norm2_selector(const std::tuple<T...>& x) ONIKA_AUTO_RET( onika::math::dot(x,x) )
-		static inline auto norm_selector(const std::tuple<T...>& x) ONIKA_AUTO_RET( std::sqrt(onika::math::norm2(x)) )
-	};
-
 
 	//===========================================================
 	//== Select tuple/non tuple operators for binary functions ==
 	//===========================================================
 	// both operands are something other than tuples
-	template <class T1,class T2> struct BinaryFunc
-	{
 #define MATH_FUNC2(name) \
-		static inline auto name##_selector(const T1& x, const T2& y) \
-		ONIKA_AUTO_RET( onika::math::name##_ss(x,y) )
+	template <class T1,class T2> struct C##name##XX \
+	{ static inline auto name##XX(const T1& x, const T2& x) ONIKA_AUTO_RET( C##name##SS::name##SS(x) ) };
 		MATH_FUNC2(sub)
 		MATH_FUNC2(add)
 		MATH_FUNC2(mul)
@@ -140,35 +143,29 @@ namespace onika { namespace math {
 	};
 
 	// second operand is a tuple and first is not
-	template <class T1, class... T2> struct BinaryFunc<T1,std::tuple<T2...> >
-	{
 #define MATH_FUNC2(name) \
-		static inline auto name##_selector(const T1& x, const std::tuple<T2...>& y) \
-		ONIKA_AUTO_RET( TSMI<std::tuple<T2...>,T1>::name##_ts(y,x) )
+	template <class T1,class... T2> struct C##name##XX<T1,std::tuple<T2...> > \
+	{ static inline auto name##XX(const T1& x, const std::tuple<T2...>& y) ONIKA_AUTO_RET( C##name##TS::name##TS(y,x) ) };
 		MATH_FUNC2(add)
 		MATH_FUNC2(mul)
 #undef 	MATH_FUNC2
 	};
 
 	// first operand is a tuple and second is not
-	template <class... T1, class T2> struct BinaryFunc<std::tuple<T1...>,T2>
-	{
 #define MATH_FUNC2(name) \
-		static inline auto name##_selector(const std::tuple<T1...>& x, const T2& y) \
-		ONIKA_AUTO_RET( TSMI<std::tuple<T1...>,T2>::name##_ts(x,y) )
+	template <class... T1, class T2> struct C##name##XX<std::tuple<T1...>,T2> \
+	{ static inline auto name##XX(const std::tuple<T1...>& x, const T2& y) ONIKA_AUTO_RET( C##name##TS::name##TS(x,y) ) };
 		MATH_FUNC2(sub)
 		MATH_FUNC2(add)
 		MATH_FUNC2(mul)
 		MATH_FUNC2(div)
 #undef 	MATH_FUNC2
-	};
 
-	template <class T1, class T2, bool EQUAL=false> struct TupleBinaryFunc {};
-	template <class T1, class T2> struct TupleBinaryFunc<T1,T2,true>
-	{
+
 #define MATH_FUNC2(name) \
-	static inline auto name##_eqtuple(const T1& x, const T2& y) \
-	ONIKA_AUTO_RET( TTMI<T1,T2>::name##_tt(x,y) )
+		template <class T1, class T2, bool EQUAL> struct C##name##TTE { int name##TTE; }; \
+		template <class T1, class T2> struct C##name##TTE<T1,T2,true> \
+		{ static inline auto name##TTE(const T1& x, const T2& y) ONIKA_AUTO_RET( C##name##TT::name##TT(x,y) ) };
 	MATH_FUNC2(sub)
 	MATH_FUNC2(add)
 	MATH_FUNC2(mul)
@@ -181,7 +178,6 @@ namespace onika { namespace math {
 
 	static inline auto distance_eqtuple(const T1& x,const T2& y)
 	ONIKA_AUTO_RET( std::sqrt(distance2_eqtuple(x,y)) )
-	};
 
 	// both operands are tuples and have the same size
 	template <class... T1, class... T2> struct BinaryFunc< std::tuple<T1...>, std::tuple<T2...> >
