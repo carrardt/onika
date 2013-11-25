6bbdcae8c48ea6ac4ced3cc3e068c76cc12ae5c8
Thierry Carrard <thierry@carrard.org>
2013-11-25 20:53:24 +0100

    keep walking ...

--- include/onika/mathfunc.h
+++ include/onika/mathfunc.h
@@ -92,6 +92,12 @@ namespace onika { namespace math {
 	#undef 	MATH_FUNC2
 
 
+
+		//==========================================================
+		//== Tuple versions of unary functions     ??             ==
+		//==========================================================
+
+
 	// tuple size helper template
 	template<class T> struct TupleLength { static constexpr int value=-1; };
 	template<class... T> struct TupleLength< std::tuple<T...> > { static constexpr int value=sizeof...(T); };
@@ -162,127 +168,25 @@ namespace onika { namespace math {
 #undef 	MATH_FUNC2
 
 
+		// both operands are tuples and have the same size
 #define MATH_FUNC2(name) \
-		template <class T1, class T2, bool EQUAL> struct C##name##TTE { int name##TTE; }; \
+		template <class T1, class T2, bool EQUAL> struct C##name##TTE { int name##TTE;/*easily recognizable error*/ }; \
 		template <class T1, class T2> struct C##name##TTE<T1,T2,true> \
-		{ static inline auto name##TTE(const T1& x, const T2& y) ONIKA_AUTO_RET( C##name##TT::name##TT(x,y) ) };
-	MATH_FUNC2(sub)
-	MATH_FUNC2(add)
-	MATH_FUNC2(mul)
-	MATH_FUNC2(div)
-	MATH_FUNC2(dot)
-#undef 	MATH_FUNC2
-
-	static inline auto distance2_eqtuple(const T1& x,const T2& y)
-	ONIKA_AUTO_RET( UnaryFunc<decltype(sub_eqtuple(x,y))>::norm2_selector( sub_eqtuple(x,y) ) )
-
-	static inline auto distance_eqtuple(const T1& x,const T2& y)
-	ONIKA_AUTO_RET( std::sqrt(distance2_eqtuple(x,y)) )
-
-	// both operands are tuples and have the same size
-	template <class... T1, class... T2> struct BinaryFunc< std::tuple<T1...>, std::tuple<T2...> >
-	{
-#define MATH_FUNC2(name) \
-	static inline auto name##_selector(const std::tuple<T1...>& x, const std::tuple<T2...>& y) \
-	ONIKA_AUTO_RET( TupleBinaryFunc<std::tuple<T1...>,std::tuple<T2...>,sizeof...(T1)==sizeof...(T2)>::name##_eqtuple(x,y) )
-	MATH_FUNC2(sub)
-	MATH_FUNC2(add)
-	MATH_FUNC2(mul)
-	MATH_FUNC2(div)
-	MATH_FUNC2(dot)
-	MATH_FUNC2(distance)
-	MATH_FUNC2(distance2)
-#undef 	MATH_FUNC2
-	};
-	//********************************************************
-
-	template <class T,unsigned int I>
-	struct TMI
-	{
-#define MATH_FUNC1(name) \
-		static inline auto name##_t(const T& x) \
-		ONIKA_AUTO_RET( TA( PTMI::name##_t(x) , onika::math::name(TG(x,I)) ) )
-		MATH_FUNC1(negate)
-		MATH_FUNC1(inverse)
-		MATH_FUNC1(abs)
-#undef 	MATH_FUNC1
-	};
-
-	template <class T>
-	struct TMI<T,0>
-	{
-#define MATH_FUNC1(name) \
-		static inline auto name##_t(const T& x) \
-		ONIKA_AUTO_RET( MT(onika::math::name(TG(x,0))) )
-		MATH_FUNC1(negate)
-		MATH_FUNC1(inverse)
-		MATH_FUNC1(abs)
-#undef 	MATH_FUNC1
-	};
-
-	template <class T1, class T2, unsigned int I>
-	struct TTMI
-	{
-#define MATH_FUNC2(name) \
-		static inline auto name##_tt(const T1& x,const T2& y) \
-		ONIKA_AUTO_RET( TA( PTTMI::name##_tt(x,y) , onika::math::name(TG(x,I),TG(y,I)) ) )
+		{ static inline auto name##TTE(const T1& x, const T2& y) ONIKA_AUTO_RET( C##name##TT::name##TT(x,y) ) }; \
+		template <class... T1, class... T2> struct C##name##XX<std::tuple<T1...>,std::tuple<T2...> > \
+		{  	static inline auto name##XX(const std::tuple<T1...>& x,const std::tuple<T2...>& y) \
+			ONIKA_AUTO_RET( C##name##TTE<std::tuple<T1...>,std::tuple<T2...>,sizeof...(T1)==sizeof...(T2)>::name##TTE(x,y) )  };
 		MATH_FUNC2(sub)
 		MATH_FUNC2(add)
 		MATH_FUNC2(mul)
 		MATH_FUNC2(div)
+		MATH_FUNC2(dot)
 #undef 	MATH_FUNC2
-		static inline auto dot_tt(const T1& x, const T2& y)
-		ONIKA_AUTO_RET( onika::math::add( PTTMI::dot_tt(x,y) , onika::math::dot(TG(x,I),TG(y,I)) ) )
-	};
-
-	template <class T1, class T2>
-	struct TTMI<T1,T2,0>
-	{
-#define MATH_FUNC2(name) \
-		static inline auto name##_tt(const T1& x,const T2& y) ONIKA_AUTO_RET( MT( onika::math::name(TG(x,0),TG(y,0)) ) )
-		MATH_FUNC2(sub)
-		MATH_FUNC2(add)
-		MATH_FUNC2(mul)
-		MATH_FUNC2(div)
-#undef 	MATH_FUNC2
-		static inline auto dot_tt(const T1& x, const T2& y) ONIKA_AUTO_RET( onika::math::dot(TG(x,0),TG(y,0)) )
-	};
-
-	template <class T1, class T2, unsigned int I>
-	struct TSMI
-	{
-#define MATH_FUNC2(name) \
-		static inline auto name##_ts(const T1& x,const T2& y) \
-		ONIKA_AUTO_RET( TA( PTSMI::name##_ts(x,y) , onika::math::name(TG(x,I),y) ) )
-		MATH_FUNC2(sub)
-		MATH_FUNC2(add)
-		MATH_FUNC2(mul)
-		MATH_FUNC2(div)
-#undef 	MATH_FUNC2
-	};
-
-	template <class T1, class T2>
-	struct TSMI<T1,T2,0>
-	{
-#define MATH_FUNC2(name) \
-		static inline auto name##_ts(const T1& x,const T2& y) ONIKA_AUTO_RET( MT( onika::math::name(TG(x,0),y) ) )
-		MATH_FUNC2(sub)
-		MATH_FUNC2(add)
-		MATH_FUNC2(mul)
-		MATH_FUNC2(div)
-#undef 	MATH_FUNC2
-	};
 
 #undef MT
 #undef TA
 #undef TG
 #undef TS
-#undef TMI
-#undef PTMI
-#undef TTMI
-#undef PTTMI
-#undef TSMI
-#undef PTSMI
 
 } }
 
