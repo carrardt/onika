620495921e40a6c252a3a064e20a37f9874816ec
Thierry Carrard <thierry@carrard.org>
2014-01-04 12:14:15 +0100

    forgot the files ...

---
+++ jitti/jitti.cpp
@@ -0,0 +1,43 @@
+//===--- tools/clang-check/ClangCheck.cpp - Clang check tool --------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+//  This file implements a clang-check tool that runs clang based on the info
+//  stored in a compilation database.
+//
+//  This tool uses the Clang Tooling infrastructure, see
+//    http://clang.llvm.org/docs/HowToSetupToolingForLLVM.html
+//  for details on setting it up with LLVM source tree.
+//
+//===----------------------------------------------------------------------===//
+
+// Declares clang::SyntaxOnlyAction.
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+// Declares llvm::cl::extrahelp.
+#include "llvm/Support/CommandLine.h"
+
+using namespace clang::driver;
+using namespace clang::tooling;
+using namespace llvm;
+
+static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
+static cl::extrahelp MoreHelp(
+    "\tPas d'options specifiques pour l'instant\n"
+    "\n"
+);
+
+int main(int argc, const char **argv)
+{
+  CommonOptionsParser OptionsParser(argc, argv);
+  ClangTool Tool(OptionsParser.getCompilations(),
+  OptionsParser.getSourcePathList());
+  return Tool.run(newFrontendActionFactory<clang::SyntaxOnlyAction>());
+}
+
