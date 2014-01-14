9747b8e8f0072a19c89b312935053354dcebfe73
Thierry Carrard <thierry@carrard.org>
2014-01-14 20:33:26 +0100

    nothing ;-)

--- jitti/Tests/main.cpp
+++ jitti/Tests/main.cpp
@@ -3,10 +3,10 @@
 int main(int argc, const char **argv)
 {
   jitti::Module* jitmodule = jitti::Compiler::createModuleFromFile(argv[1],"-DMYDEFINE=33");
-  auto justdoit = jitmodule->getFunction(argv[2]);
+  auto function = jitmodule->getFunction(argv[2]);
 
-  justdoit( "Hello!" , 666 );
+  function( "Hello!" , 666 );
 
-  return justdoit.getReturnValueAsULong();
+  return function.getReturnValueAsULong();
 }
 
